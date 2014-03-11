5b707cd3bfdcc6a64e9655f8ebf806ceed1f6820
Melissa Canivel <mcanivel@ucsd.edu>
2014-03-11 11:56:47 -0700

    Added fixed nav and kinda fixed link scroll

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 //Our main javascript file
+
+$(document).ready(function() {
+	var stickyNavTop = $('#Nav').offset().top;
+
+	var stickyNav = function() {
+		var scrollTop = $(window).scrollTop();
+
+		if (scrollTop > stickyNavTop) {
+			$('#Nav').addClass('sticky');
+		} else {
+			$('#Nav').removeClass('sticky');
+		}
+	};
+
+	stickyNav();
+
+	$(window).scroll(function() {
+		stickyNav();
+	});
+});

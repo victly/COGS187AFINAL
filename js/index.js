be5c034f03528711dff2e28ab56a328db51266e6
Melissa Canivel <mcanivel@ucsd.edu>
2014-03-15 23:12:54 -0700

    Got rid of the JS for sticky nav
    Now the main nag is just sticky
    Padding added to shift page downward

--- js/index.js
+++ js/index.js
@@ -1,24 +1,6 @@
 //Our main javascript file
 
 $(document).ready(function() {
-	var stickyNavTop = $('#Nav').offset().top;
-
-	var stickyNav = function() {
-		var scrollTop = $(window).scrollTop();
-
-		if (scrollTop > stickyNavTop) {
-			$('#Nav').addClass('sticky');
-		} else {
-			$('#Nav').removeClass('sticky');
-		}
-	};
-
-	stickyNav();
-
-	$(window).scroll(function() {
-		stickyNav();
-	});
-
 
 	$(".fancybox-thumb").fancybox({
 		prevEffect	: 'none',
